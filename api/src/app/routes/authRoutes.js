e26ea0ee2d0e1fd981b7dd9f27c65bdc061d90ba
lancao2 <lancao.af@gmail.com>
2024-04-18 19:39:05 -0300

    feat: login admin

--- api/src/app/routes/authRoutes.js
+++ api/src/app/routes/authRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const routes = express.Router();
 
 const authController = require('../controllers/authController');
+const adminMiddleware = require("../middleware/admin")
 
 routes.post('', authController.store)
+routes.post('/admin',adminMiddleware, authController.store)
 
 module.exports = routes
